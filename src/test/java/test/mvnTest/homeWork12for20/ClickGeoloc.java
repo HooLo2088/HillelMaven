d26d476df7f58ccb50cb102b18d3069e884a4b05
HooLo <hoolohi@gmail.com>
2022-01-15 17:39:21 +0200

    HomeWork 14 For Lesson 22 Refactoring cod from 12 homeWork

--- src/test/java/test/mvnTest/homeWork12for20/ClickGeoloc.java
+++ src/test/java/test/mvnTest/homeWork12for20/ClickGeoloc.java
@@ -5,29 +5,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ClickGeoloc {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void ClickGeo() {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-
         driver.get("https://the-internet.herokuapp.com/geolocation");
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
 
         driver.findElement(By.xpath("//*[@id=\"content\"]/div/button")).click();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"lat-value\"]")));
 
         WebElement lalitude = driver.findElement(By.xpath("//*[@id=\"lat-value\"]"));
         WebElement longitude = driver.findElement(By.xpath("//*[@id=\"long-value\"]"));
         System.out.printf("Lalitude = %s%nLongitude = %s", lalitude.getText(), longitude.getText());
+    }
 
+    @AfterClass
+    public void after() {
         driver.quit();
     }
 }
--- src/test/java/test/mvnTest/homeWork12for20/WebLogin.java
+++ src/test/java/test/mvnTest/homeWork12for20/WebLogin.java
@@ -7,44 +7,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class WebLogin {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void wdLogin() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
 
         driver.get("https://the-internet.herokuapp.com/login");
 
         driver.findElement(By.id("username")).sendKeys("tomsmith");
 
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         driver.findElement(By.xpath("//*[@id=\"login\"]/button/i")).click();
 
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         WebElement success = driver.findElement(By.cssSelector("#flash"));
-        Assert.assertTrue(success.getText().contains("You logged into a secure area!"), "good cred)))");
+        Assert.assertTrue(success.getText().contains("You logged into a secure area!"), "bed cred)))");
+    }
 
+    @AfterClass
+    public void after() {
         driver.quit();
     }
 }
